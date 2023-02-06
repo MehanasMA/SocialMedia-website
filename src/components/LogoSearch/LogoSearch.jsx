ec3d0227bcbb53c041dd89d076b111fea2925b2b
Mehanas M A <mamehanas88@gmail.com>
2023-02-06 10:46:41 +0530

    first commit

--- src/components/LogoSearch/LogoSearch.jsx
+++ src/components/LogoSearch/LogoSearch.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import logo from '../../img/logo.png'
-import './LogoSearch.css'
- 
-import {UilSearch} from '@iconscout/react-unicons'
+import React from 'react';
+import logo from '../../img/logo.png';
+import './LogoSearch.css';
+
+import { UilSearch } from '@iconscout/react-unicons';
 
 const LogoSearch = () => {
-  return (
-    <div className='LogoSearch'>
-     <img src={logo} alt="" />
-    <div className='Search'>
-        <input type="text" placeholder='#Explore' />
-        <div className='search-icon'>
-            <UilSearch/>
-        </div>
-    </div>
-    </div>
-  )
-}
+	return (
+		<div className="LogoSearch">
+			<img src={logo} alt="" />
+			<div className="Search">
+				<input type="text" placeholder="#Explore" />
+				<div className="search-icon">
+					<UilSearch />
+				</div>
+			</div>
+		</div>
+	);
+};
 
-export default LogoSearch
+export default LogoSearch;
